c7564b3615c91e92960a8fcf95da7322e2323687
Silviu Lung <slung@maporama.com>
2013-03-25 17:49:47 +0200

    - [ADD] Support for Ajax Login - [ADD] Support for validating login inputs

--- src/models/AjaxManager.js
+++ src/models/AjaxManager.js
@@ -9,176 +9,34 @@
 	 * 
 	 * @constructor Ajax
 	 */
-	function Ajax()
-	{
-		//this.createXhr();
-	}
-	
-	Ajax.prototype = {
-			
-		/**
-		 * Creates a new XHR request
-		 * depending on the browser. 
-		 * 
-		 * @method createXhr
-		 * @param options {Object}
-		 */
-		createXhr: function( options )
-		{
-			if( options )
-				this.options = options;
-			
-			var ajax = this;
-			
-			//Don't create XDR object if the url points to the origin
-			//IE doesn't like to this
-			if( this.isIE() && !this.isSameWithOrigin(this.url) )
-			{
-				if( this.xhr )
-					this.xhr.abort();
-				
-				this.xhr = new XDomainRequest();
-				this.xhr.onload = function(){ ajax.onComplete(); };
-			    this.xhr.onerror = function() { ajax.onError(); };
-				
-			}
-			else
-			{
-				if (!!window.XMLHttpRequest) 
-				{
-					this.xhr = new window.XMLHttpRequest(); // Most browsers
-				}
-				else if (!!window.ActiveXObject) 
-				{
-					this.xhr = new window.ActiveXObject('Microsoft.XMLHTTP'); // Some IE
-				}
-			}
-			
-			if( !this.xhr )
-				throw new Error("Unable to create XHR object!");
-			
-			
-			var ajax = this;
-			
-			this.xhr.onreadystatechange = function () 
-			{
-				 if (this.readyState === 4) 
-				 {
-					if (this.status >= 400)
-						ajax.onError();
-					else
-						ajax.onComplete();
-				  }
-			 };
-			 
-			 this.xhr.onprogress = function()
-			 {
-				 
-			 };
-			 
-			 this.xhr.ontimeout = function()
-			 {
-			 	throw new Error("timeout");
-			 };
-			 
-			 return;
-		},
+	var Ajax = new Class({
 		
-		abort: function()
-		{
-			this.xhr.abort();
-		},
+		geoAdsPlatformUrl = "http://127.0.0.1:1314/",
 		
-		/**
-		 * Helper function for IE check.
-		 * 
-		 * @method isIE
-		 */
-		isIE: function()
+		login: function( email, password, success, error )
 		{
-			if (navigator.appVersion.indexOf("MSIE") != -1)
-				return true;
+			var url = this.geoAdsPlatformUrl + "login";
+			var data = email + "&" + password;
 			
-			return false;
+			jQuery.ajax({
+		    	url: url,
+		    	type: 'POST',
+		    	data: data,
+		    	success: GA.bind(function( data )
+		    	{
+		    		if( success )
+		            	success.apply( this, [GA.JSON.parse(data)] );
+		    	}, this),
+		    	error: GA.bind(function( data )
+		    	{
+		    		if( error )
+		            	error.apply( this, [] );
+		    	}, this);
+		    });
 		},
-		
-		/**
-		 * Helper function to compare the domains of two URLs
-		 * 
-		 * @method isSameWithOrigin
-		 */
-		isSameWithOrigin: function ( url )
-		{
-			var reg = new RegExp("^http(s)?://.*?(/|$)","i");
-			
-			var newHost = reg.exec(url)[0];
-			var originHost = reg.exec( window.location.href)[0];
-			
-			
-			return  ( originHost === newHost );
-		},
-		
-		/**
-		 * Send XHR request to server.
-		 * 
-		 * @param url
-		 * @param options
-		 */
-		send: function( url, options )
-		{
-			this.options = options;
-			this.url = url;
-			
-			if( !this.xhr )
-				this.createXhr();
-			
-		    this.xhr.open(options.method, url, true);
-		    
-		    // IE needs timeout set after xhr.open()
-		    this.xhr.timeout = 20000;
-		    
-		    //Use credentials 
-		    if(this.xhr.withCredentials !== undefined)
-		    	if( options.withCredentials )
-		    		this.xhr.withCredentials  = options.withCredentials;
-		    
-		    // IE XDR doesn't support request headers
-		    if(this.xhr.setRequestHeader && options.headers)
-		    	for( var i = 0; i < options.headers.length ; i++ )
-		    		this.xhr.setRequestHeader(  options.headers[i].name , options.headers[i].value  );
-		    
-		    //Data payload
-		    if(options.data)
-		    	this.xhr.send(options.data);
-		    else
-		    	this.xhr.send(null);
-		   
-		    
-		    function timeoutHandler() {
-		        throw new Error('Loading timeout: ' + url);
-		    };
-		    
-		    return this;
-		},
-		
-		onComplete: function()
-		{
-			if( this.options.on.success )
-				this.options.on.success( this.xhr.responseText );
-		},
-		
-		onError: function()
-		{
-			if( this.options.on.error )
-				this.options.on.error(this.xhr.responseText);
-		}	
-	};
-	
+	});
 	// Create & add an instance of ajax to GeoAds namespace
-	GA.ajax = function( url, confg )
-	{
-		return new Ajax().send(url, confg);
-	};
+	GA.Ajax = Ajax;
 	
 })(GA);
 
--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -1,5 +1,10 @@
 (function( GA )
 {
+	var EMAIL_INPUT_SELECTOR = "#login-email";
+	var PASSWORD_INPUT_SELECTOR = "#login-pass";
+	var INPUT_ERROR_CLASS = "input-error";
+	var LOGIN_ERROR_MSG = "";
+	
 	var LoginView = GA.View.extend({
 		
 		events: {
@@ -31,6 +36,38 @@
 			return this;
 		},
 		
+		getEmail: function()
+		{
+			return GA.one( EMAIL_INPUT_SELECTOR, this.container ).value;
+		},
+		
+		getPassword: function()
+		{
+			return GA.one( PASSWORD_INPUT_SELECTOR, this.container ).value;
+		},
+		
+		validateEmailInput: function()
+		{
+			var email = this.getEmail();
+			
+			if ( email == "" )
+				return false;
+				
+			var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+			
+			return re.test(email);
+		},
+		
+		validatePasswordInput: function()
+		{
+			var password = this.getPassword();
+			
+			if ( password == "" )
+				return false;
+			
+			return true;
+		},
+		
 		/*
 		 * Messages
 		 */
@@ -47,7 +84,40 @@
 		
 		onLoginSubmitClick: function( evt )
 		{
+			var emailValid = false;
+			var passwordValid = false;
 			
+			if ( !this.validateEmailInput() )
+			{
+				GA.addClass( GA.one( EMAIL_INPUT_SELECTOR, this.container ), INPUT_ERROR_CLASS );
+				emailValid = false;				
+			}
+			else
+			{
+				GA.removeClass( GA.one( EMAIL_INPUT_SELECTOR, this.container ), INPUT_ERROR_CLASS );
+				emailValid = true;
+			}
+				
+			if ( !this.validatePasswordInput() )
+			{
+				GA.addClass( GA.one( PASSWORD_INPUT_SELECTOR, this.container ), INPUT_ERROR_CLASS );
+				passwordValid = false;				
+			}
+			else
+			{
+				GA.removeClass( GA.one( PASSWORD_INPUT_SELECTOR, this.container ), INPUT_ERROR_CLASS );
+				passwordValid = true;
+			}
+			
+			if ( !emailValid || !passwordValid )
+				return;
+				
+			//Call login system
+			GA.Ajax.login( this.getEmail(), this.getPassword(), function(){
+				//On success, hide Sign in and Sign up labels on Menu
+			}, function(){
+				//On error, activate error msg
+			} );
 		},
 		
 		onLostPasswordClick: function( evt )
